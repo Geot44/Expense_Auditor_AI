9cd1d75d11be56568cbcac7014e8ad99ec662a8f
George Thomas <147007612+Geot44@users.noreply.github.com>
2026-04-08 00:26:27 +0000

    Removed old files

--- test_models.py
+++
@@ -1,7 +0,0 @@
-import google.generativeai as genai
-# Paste your key here just for the test
-genai.configure(api_key="YOUR_KEY_HERE")
-
-for m in genai.list_models():
-  if 'generateContent' in m.supported_generation_methods:
-    print(m.name)
